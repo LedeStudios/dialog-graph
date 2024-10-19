e07cfad31bd5578f473f1060c0c749bf4b0034c3
lede-dev <lede.7716@gmail.com>
2024-10-20 06:23:43 +0900

    chore: setup project

---
+++ Source/Dialog/Dialog.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Dialog.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Dialog, "Dialog" );
