21a26bb86908d7c393c3a720c0b40b43f0ff84b2
lede-dev <lede.7716@gmail.com>
2024-10-20 11:01:19 +0900

    feat: create data asset

---
+++ Plugins/DialogGraph/Source/DialogGraph/Data/Dialog.h
@@ -0,0 +1,16 @@
+﻿// Copyright Lede Studios. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "Dialog.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DIALOGGRAPH_API UDialog : public UObject
+{
+	GENERATED_BODY()
+};
