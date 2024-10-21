73c2b5d01c0144c4ee34b30838d9889f1e011f46
lede-dev <lede.7716@gmail.com>
2024-10-21 09:38:26 +0900

    feat: add test dialog data

--- Plugins/DialogGraph/Source/DialogGraph/Data/Dialog.h
+++ Plugins/DialogGraph/Source/DialogGraph/Data/Dialog.h
@@ -6,11 +6,34 @@
 #include "UObject/Object.h"
 #include "Dialog.generated.h"
 
-/**
- * 
- */
+class UDialogNode;
+
 UCLASS()
 class DIALOGGRAPH_API UDialog : public UObject
 {
 	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, Category="Dialog")
+	TObjectPtr<UDialogNode> Node;
+	
 };
+
+
+UCLASS()
+class UDialogNode : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
+	FText Speaker;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
+	FText Line;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Node")
+	TObjectPtr<UDialogNode> NextNode;
+	
+};
+
